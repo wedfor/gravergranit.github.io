476cfceba7b98466d23b4a22efb5a018dc793ed9
wedfor <87817614+wedfor@users.noreply.github.com>
2024-08-04 13:19:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+
+// Получаем элементы слайдера
+const slider = document.querySelector('.slider');
+const prevButton = document.querySelector('.arrow1');
+const nextButton = document.querySelector('.arrow2');
+const slides = Array.from(slider.querySelectorAll('.slider > img'));
+const descriptions = Array.from(document.querySelectorAll('.description-stage'));
+const slideCount = slides.length;
+let slideIndex = 0;
+
+// Устанавливаем обработчики событий для кнопок
+prevButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
+
+// Функция для показа предыдущего слайда
+function showPreviousSlide() {
+  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+  updateSlider();
+}
+
+// Функция для показа следующего слайда
+function showNextSlide() {
+  slideIndex = (slideIndex + 1) % slideCount;
+  updateSlider();
+}
+
+// Функция для обновления отображения слайдера
+function updateSlider() {
+  slides.forEach((slide, index) => {
+    if (index === slideIndex) {
+      slide.style.display = 'block';
+    } else {
+      slide.style.display = 'none';
+    }
+  });
+
+  descriptions.forEach((description, index) => {
+    if (index === slideIndex) {
+      description.style.display = 'block';
+    } else {
+      description.style.display = 'none';
+    }
+  });
+  
+}
+
+
+// Инициализация слайдера
+updateSlider();
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const backToTop = document.getElementById("back-to-top");
+ 
+  // Показать/скрыть кнопку при прокрутке страницы
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 300) {
+      backToTop.style.display = "block";
+    } else {
+      backToTop.style.display = "none";
+    }
+  });
+ 
+  // Плавная прокрутка при клике на кнопку
+  backToTop.addEventListener("click", function (event) {
+    event.preventDefault();
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+});
